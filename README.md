ea20711b2d16aeb1ddec619b3e953ba3b2c2009f
Rahul.Bhave <rahul.bhave@qxf2.com>
2020-08-05 16:09:50 +0530

    added highlighting for questions + README.md

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
+---------
 # hackthon-2020-url-exercise
-hackthon-2020-url-exercise
+--------
 
-This repo reprsents hackthon work done.
+This repo reprsents hackthon work done. This exercise will help to build understanding of guessing URL endpoints.
 
-This repo contains two exercises:
-1. Objective questions- Run questions.py and it will guide you to further question sets.
-2. Subjective questions- Run answer_type_question.py. Questions will be prompted to the user and user will write answers.
+
+# How to run the exercise:
+
+1. __questions.py__ Run the questions.py using command `python questions.py`, this will prompt you objective questions. Answer the questions on the console. At the end of the exercise correct and incorrect answers will be marked and scores will be declared.
+
+2. __quiz.json__ All the quiz questions will be stored in the in this json. In case you want to add new question, you can add it in this file.
--- question.py
+++ question.py
@@ -2,10 +2,26 @@
 Quiz interface
 help taken from followng:
 # https://github.com/cherryWood55/Quiz-Game
+# https://stackoverflow.com/questions/287871/how-to-print-colored-text-in-python
 
 """
 import json
 import time
+import os
+
+os.system("")
+
+class style():
+    BLACK = '\033[30m'
+    RED = '\033[31m'
+    GREEN = '\033[32m'
+    YELLOW = '\033[33m'
+    BLUE = '\033[34m'
+    MAGENTA = '\033[35m'
+    CYAN = '\033[36m'
+    WHITE = '\033[37m'
+    UNDERLINE = '\033[4m'
+    RESET = '\033[0m'
 
 #qusetion list
 QUESTION_LIST = ['quiz']
@@ -14,14 +30,14 @@ def post_question(question):
     """
     Method to post question
     """
-    print("your question \n" + question)
-    answer = input("Enter Your answer [a/b/c/d]: ")
+    print(style.YELLOW +  "your question \n" + style.UNDERLINE + question)
+    answer = input(style.GREEN + "Enter Your answer [a/b/c/d]: ")
     while(True):
         if answer.lower() in ['a', 'b', 'c', 'd']:
             return answer
         else:
-            print("Invalid answer. Enter again")
-            answer = input("Enter answer [a/b/c/d]: ")
+            print(style.GREEN + "Invalid answer. Enter again")
+            answer = input(style.GREEN + "Enter answer [a/b/c/d]: ")
 
 def mark_question(key, value):
     """
@@ -29,12 +45,12 @@ def mark_question(key, value):
     """
     actual = value["answer"]
     if value["user_response"].lower() == actual.lower():
-        print("Q.{0} Correct!\n".format(key))
+        print(style.GREEN + "Q.{0} Correct!\n".format(key))
         return 2
     else:
-        print("Q.{0} Incorrect!".format(key))
-        print("Correct Answer is ({0})".format(actual))
-        print ("Learn more : " + value["more_info"] + "\n")
+        print(style.RED + "Q.{0} Incorrect!".format(key))
+        print(style.GREEN + "Correct Answer is ({0})".format(actual))
+        print (style.CYAN + "Learn more : " + value["more_info"] + "\n")
         return 0
 
 def take_test(questions):
@@ -42,14 +58,14 @@ def take_test(questions):
     Method to take questions
     """
     mark = 0
-    print("Instructions :\n1. Only one valid answer.\n2. Correct answer 2 marks\n3. Wrong answer 0 marks \n")
+    print(style.CYAN + "Instructions :\n1. Only one valid answer.\n2. Correct answer 2 marks\n3. Wrong answer 0 marks \n")
     # time.sleep(2)
     for key, value in questions.items():
         questions[key]["user_response"] = post_question(value["question"])
     print("\n---------- PRINTING RESULT----------\n")
     for key, value in questions.items():
         mark += mark_question(key, value)
-    print("Your mark:", mark, "/", (2 * len(questions)))
+    print(style.GREEN + "Your mark:", mark, "/", (2 * len(questions)))
 
 def load_question(filename):
     """
